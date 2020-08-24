e24f06d2acff76e1ed626a2a65b6f038a2720581
luke McCue <lukemccue33@gmail.com>
2020-08-24 16:18:59 -0500

    gnertated and displayed a password

--- script.js
+++ script.js
@@ -23,48 +23,47 @@ var values = {
 
 function generatePassword() {
   values.charLength = parseInt(prompt("How many characters do you want your password to be?"));
+  if ( isNaN(values.charLength) || values.charLength<8 || values.charLength>128) {
+    alert("please enter a valid number between 8 and 128.");
+    return;
+  }
   values.includeUpper = confirm("Any uppercase letters?");
   values.includeLower = confirm("Any lowercase letters?");
   values.includeNumber = confirm("Any numbers in your password?");
   values.includeSpecial = confirm("any special characters?");
+
+  var upperOP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerOP = "abcdefghijklmnopqrstuvwxyz"
+  var numberOP = "0123456789"
+  var specialOP = "!@#$%^&*<>?+_~"
+
+  var importantCharacters = [];
+
+  if (values.includeUpper) {
+    importantCharacters = importantCharacters.concat(upperOP.split(""));
+  }
+
+  if (values.includeLower) {
+    importantCharacters = importantCharacters.concat(lowerOP.split(""));
+  }
+
+  if (values.includeNumber) {
+    importantCharacters = importantCharacters.concat(numberOP.split(""));
+  }
+
+  if (values.includeSpecial) {
+    importantCharacters = importantCharacters.concat(specialOP.split(""));
+  }
+
+  console.log(importantCharacters);
+  console.log(values.charLength);
+  var passwordText = "";
+
+  for (var i = 0; i < values.charLength; i++) {
+   var randIndex = Math.floor(Math.random() * importantCharacters.length);
+   console.log(importantCharacters[randIndex]); 
+   passwordText += importantCharacters[randIndex];
+  }
+  return passwordText;
+
 }
-
-console.log(values.charLength);
-console.log(values.includeUpper);
-console.log(values.includeLower);
-console.log(values.includeNumber);
-console.log(values.includeSpecial);
-
-
-var upperOP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerOP = "abcdefghijklmnopqrstuvwxyz"
-var numberOP = "0123456789"
-var specialOP = "!@#$%^&*<>?+_~"
-
-var importantCharacters = [];
-
-if (values.includeUpper) {
-  importantCharacters = importantCharacters.concat(upperOP.split(""));
-}
-
-if (values.includeLower) {
-  importantCharacters = importantCharacters.concat(lowerOP.split(""));
-}
-
-if (values.includeNumber) {
-  importantCharacters = importantCharacters.concat(numberOP.split(""));
-}
-
-if (values.includeSpecial) {
-  importantCharacters = importantCharacters.concat(specialOP.split(""));
-}
-
-console.log(importantCharacters);
-console.log(values.charLength);
-var passwordText = "";
-
-for (var i = 0; i < values.charLength; i++) {
-  console.log(i);
-}
-
-
